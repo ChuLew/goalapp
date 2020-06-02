9ed5a2ece5402f174f4470151d3436988f05b99e
Mitch Lew <mitchginlew@gmail.com>
2020-06-01 20:49:03 -0400

    Updates

---
+++ precache-manifest.4fee44d04f7de7c855ff02467ec7e2ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "075a34b22b5598dd4573cad5c2357a21",
+    "url": "/goalapp/index.html"
+  },
+  {
+    "revision": "8e00bf25f46fd1414cbc",
+    "url": "/goalapp/static/css/2.9277e48d.chunk.css"
+  },
+  {
+    "revision": "d908e5474dac51e5d327",
+    "url": "/goalapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8e00bf25f46fd1414cbc",
+    "url": "/goalapp/static/js/2.f7d5deb0.chunk.js"
+  },
+  {
+    "revision": "ce8ac3c5d9da37689de19b45a8b43043",
+    "url": "/goalapp/static/js/2.f7d5deb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d908e5474dac51e5d327",
+    "url": "/goalapp/static/js/main.c4021164.chunk.js"
+  },
+  {
+    "revision": "ecf733a87a299d7827ac",
+    "url": "/goalapp/static/js/runtime-main.49b557a6.js"
+  }
+]);
