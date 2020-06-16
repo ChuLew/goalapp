1105b603eb7a93450ca11eb733a4961cb1a3a9a1
Mitch Lew <mitchginlew@gmail.com>
2020-06-15 22:52:00 -0400

    added pie chart

--- src/Expenses.js
+++ src/Expenses.js
@@ -6,15 +6,18 @@ import "./App.css";
 import {Table,Container,Input,Button,Label,Form,FormGroup} from 'reactstrap';
 import {Link} from 'react-router-dom';
 import Moment from 'react-moment';
+import CanvasJSReact from './assets/canvasjs.react';
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 class Expenses extends Component {
 
-
+    
    emptyItem = {
         description : '' ,
         expensedate : new Date(),
         id:'',
         location : '',
-        category : {id:1 , name:'Travel'}
+        category : {id:1 , name:'Travel'},
+        price : ''
     }
     constructor(props){
         super(props)
@@ -24,7 +27,8 @@ class Expenses extends Component {
           categories:[],
           Expenses : [],
           date :new Date(),
-          item : this.emptyItem
+          item : this.emptyItem,
+          price :[]
          }
   
          this.handleSubmit= this.handleSubmit.bind(this);
@@ -54,8 +58,6 @@ class Expenses extends Component {
       
       }
       handleCatChange(category) {
-        const target = category.target;
-        const value = target.value;
         const idx = category.target.selectedIndex; 
         let idex = category.target.selectedIndex;
         let dataset = category.target.options[idex].text;
@@ -114,10 +116,9 @@ class Expenses extends Component {
 
         
     }
-
-    
     render() { 
-        const title = <h3>Add Expense</h3>;
+       
+        const title = <h2>Add Expense</h2>;
         const {categories} = this.state;
         const {Expenses,isLoading} = this.state;
         if (isLoading)
@@ -135,45 +136,51 @@ class Expenses extends Component {
                     <td>{expense.location}</td>
                     <td><Moment date={expense.expensedate} format="YYYY/MM/DD"/></td>
                     <td>{expense.category.name}</td>
+                    <td>{expense.price}</td>
                     <td><Button size="sm" color="danger" onClick={() => this.remove(expense.id)}>Delete</Button></td>
-
                 </tr> )
+        let sum_value = Expenses.reduce((sum, current)=>{
+            return sum + current.price
+        }, 0);
+       // need to add proccessing where each of these percentages are calcuated
+        let Grocery = 18;
+        let Housing = 49;
+        let Enjoyment = 9;
+        let Vacation = 5;
+        let Transportation = 19;
+        const options = {
+			exportEnabled: true,
+			animationEnabled: true,
+			title: {
+				text: "Expense Percentages"
+			},
+			data: [{
+				type: "pie",
+				startAngle: 75,
+				toolTipContent: "<b>{label}</b>: {y}%",
+				showInLegend: "true",
+				legendText: "{label}",
+				indexLabelFontSize: 16,
+				indexLabel: "{label} - {y}%",
+				dataPoints: [
+					{ y: Grocery, label: "Grocery/Food" },
+					{ y: Housing, label: "Housing/Utilities" },
+					{ y: Enjoyment, label: "Entertainment/Enjoyment" },
+					{ y: Vacation, label: "Vacation" },
+					{ y: Transportation, label: "Transportation" }
+				]
+			}]
+		}
         return (
             <div><AppNav/>
+			<CanvasJSChart options = {options} 
+				/* onRef={ref => this.chart = ref} */
+			/>
+			{/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
             <Container>
-                {title}
-                <Form onSubmit = {this.handleSubmit}>
-                    <FormGroup>
-                        <Label for="description">Description of Expense</Label>
-                        <Input type = "description" name = "description" id = "description"
-                         onChange={this.handleChange} autoComplete = "name"/>
-                    </FormGroup>
-                    <FormGroup>
-                        <Label for="category">Category</Label>
-                        <select onChange={this.handleCatChange}>
-                                {optionList}
-                        </select>
-                    </FormGroup>
-                    <FormGroup>
-                        <Label for="city">Date</Label>
-                        <DatePicker selected={this.state.item.expensedate}  onChange={this.handleDateChange} />
-                    </FormGroup>
-                    <div className = "row">
-                        <FormGroup className = "col-md-4 mb-3">
-                            <Label for ="location">Location</Label>
-                            <Input type="text" name="location" id="location" onChange={this.handleChange}/>
-                        </FormGroup>
-                    </div>
-                    <FormGroup>
-                        <Button color = "primary" type = "submit">Save</Button>
-                        <Button color = "secondary" tag = {Link} to ="/">Cancel</Button>
-                    </FormGroup>
-                    
-                </Form>
-            </Container>
-
-            <Container>
-                <h3>Expense List</h3>
+                <div className="center">
+                <h3>Total Expenses:${sum_value}</h3>
+                </div>
             <Table className = "mt-4">
                 <thead>
                     <tr>
@@ -181,6 +188,7 @@ class Expenses extends Component {
                         <th width = "10%">Location</th>
                         <th width >Date</th>
                         <th width >Category</th>
+                        <th width > Price </th>
                         <th width = "10%">Action</th> 
                     </tr>
                 </thead>
@@ -189,8 +197,36 @@ class Expenses extends Component {
                 </tbody>
             </Table>
             </Container>
-
-
+            <Container>
+                {title}
+                <Form onSubmit = {this.handleSubmit}>
+                    <FormGroup className = "col-md-4 mb-3">
+                        <div className = "sidebyside">
+                        <Label for="description">Description of Expense</Label>
+                        <Input type = "description" name = "description" id = "description"
+                         onChange={this.handleChange} autoComplete = "name"/>
+                         </div>
+                        <Label for ="location">Location</Label>
+                        <Input type="text" name="location" id="location" onChange={this.handleChange}/>
+                        <Label for="price">Price</Label>
+                        <Input type="number" name="price" id="price" onChange={this.handleChange}/>
+                        <Label for="category">Category</Label>
+                        <select onChange={this.handleCatChange}>
+                                {optionList}
+                        </select>
+                        <div>
+                        <Label for="city">Date</Label>
+                        <DatePicker selected={this.state.item.expensedate}  onChange={this.handleDateChange} />
+                    
+                        </div>
+                           </FormGroup>
+                   
+                    <FormGroup>
+                        <Button color = "primary" type = "submit">Save</Button>
+                        <Button color = "secondary" tag = {Link} to ="/">Cancel</Button>
+                    </FormGroup>
+                </Form>
+            </Container>
             </div>
             );
     }
